843f42cea8c395bfa90868ff76bcc18c5af64bb8
PaliJunctureSeparator <162350300+PaliJunctureSeparator@users.noreply.github.com>
2024-04-18 13:09:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -12,24 +12,9 @@ function data(result, entry){
     }else{
         wrapper.classList.add("active");
         let definitions = result[0].meanings[0].definitions[0],
-        phontetics = `${result[0].meanings[0].partOfSpeech}  /${result[0].phonetics[0].text}/`;
         document.querySelector(".entry p").innerText = result[0].entry;
-        document.querySelector(".entry span").innerText = phontetics;
         document.querySelector(".meaning span").innerText = definitions.definition;
         document.querySelector(".example span").innerText = definitions.example;
-        audio = new Audio(result[0].phonetics[0].audio);
-
-        if(definitions.synonyms[0] == undefined){
-            synonyms.parentElement.style.display = "none";
-        }else{
-            synonyms.parentElement.style.display = "block";
-            synonyms.innerHTML = "";
-            for (let i = 0; i < 5; i++) {
-                let tag = `<span onclick="search('${definitions.synonyms[i]}')">${definitions.synonyms[i]},</span>`;
-                tag = i == 4 ? tag = `<span onclick="search('${definitions.synonyms[i]}')">${definitions.synonyms[4]}</span>` : tag;
-                synonyms.insertAdjacentHTML("beforeend", tag);
-            }
-        }
     }
 }
 
